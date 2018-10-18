8e25ec25ada1cbcea073dfc464182d8cd19cade2
VyunSergey <vyun.sergey@icloud.com>
2018-10-18 11:22:50 +0300

    Adding Headers reading from files
    Signed-off-by: VyunSergey <vyun.sergey@icloud.com>

--- MappingEKStoS2T.py
+++ MappingEKStoS2T.py
@@ -6,28 +6,22 @@ import pandas as pd
 if __name__ == '__main__':
     files_path = 'C:\\Users\\User\\PycharmProjects\\MappingEKStoS2T\\'
 
-    EKS_tables_file = 'classes.csv'
-    EKS_tables_file_col_names = [
-        'id', 'name', 'entity_id', 'parent_id', 'has_instances', 'init_state_id', 'short_name', 'agregate',
-        'base_class_id', 'target_class_id', 'criteria_id', 'data_size', 'data_precision', 'min_value', 'max_value',
-        'default_value', 'autonomous', 'is_collection', 'modified', 'pad_style', 'pad_char', 'pad_length', 'interface',
-        'kernel', 'param_group', 'tag', 'validator_id', 'input_mask', 'trigger_id', 'storage_group', 'init_method_id',
-        'lob_storage_group', 'temp_type', 'has_type', 'key_attr', 'data_precision_min', 'properties', 'ods_validfrom',
-        'ods_opc', 'ods_sequenceid'
-    ]
+    EKS_tables_file_name = 'classes.csv'
+    with open(file=files_path + EKS_tables_file_name, mode='r', encoding='utf-8') as EKS_tables_file:
+        EKS_tables_file_header = EKS_tables_file.readline()
+    EKS_tables_file_col_names = EKS_tables_file_header.replace('\n', '').split(';')
 
-    EKS_columns_file_ = 'classes_attributes.csv'
-    EKS_columns_file_col_names = [
-        'CLASS_ID', 'ATTR_ID', 'SELF_CLASS_ID', 'NAME', 'REQUIRED', 'POSITION', 'FILTERABLE', 'BROWSEABLE', 'SEQUENCED',
-        'MODIFIED'
-    ]
+    EKS_columns_file_name = 'classes_attributes.csv'
+    with open(file=files_path + EKS_columns_file_name, mode='r', encoding='utf-8') as EKS_columns_file:
+        EKS_columns_file_header = EKS_columns_file.readline()
+    EKS_columns_file_col_names = EKS_columns_file_header.replace('\n', '').split(';')
 
-    EKS_tables_df = pd.read_csv(filepath_or_buffer=files_path+EKS_tables_file, delimiter=';', header=1,
+    EKS_tables_df = pd.read_csv(filepath_or_buffer=files_path + EKS_tables_file_name, delimiter=';', header=1,
                                 names=EKS_tables_file_col_names, index_col=None, encoding='utf-8')
     EKS_tables_df.fillna('', inplace=True)
     print(EKS_tables_df)
 
-    EKS_columns_df = pd.read_csv(filepath_or_buffer=files_path+EKS_columns_file_, delimiter=';', header=1,
+    EKS_columns_df = pd.read_csv(filepath_or_buffer=files_path + EKS_columns_file_name, delimiter=';', header=1,
                                  names=EKS_columns_file_col_names, index_col=None, encoding='utf-8')
     EKS_columns_df.fillna('', inplace=True)
     print(EKS_columns_df)
